cac9f6ad61131e06857d4ea542bc45367d734144
discphy <dinophy@nate.com>
2025-10-01 22:00:46 +0900

    feat: 플레이어 도메인 작성

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/player/Player.java
@@ -0,0 +1,17 @@
+package com.loopers.domain.player;
+
+import com.loopers.domain.team.Team;
+import com.loopers.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Player {
+
+    private User user;
+
+    private Team team;
+
+
+
+}
