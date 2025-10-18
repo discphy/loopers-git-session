cba1bee113c1a16baf42b118458ecbc773b7d9da
10 <dinophy@nate.com>
2025-10-18 21:05:25 +0900

    fix: 회원 가입 시 회원명, 비밀번호 정책 수정

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -14,12 +14,12 @@ public class UserService {
             throw new IllegalArgumentException("이미 존재하는 회원 이름입니다.");
         });
 
-        if (name == null) {
+        if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("회원 이름이 올바르지 않습니다.");
         }
 
-        if (password == null || password.length() < 8) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상이어야 합니다.");
+        if (password == null || password.length() < 10) {
+            throw new IllegalArgumentException("비밀번호는 최소 10자 이상이어야 합니다.");
         }
 
         User user = new User(name, password);
